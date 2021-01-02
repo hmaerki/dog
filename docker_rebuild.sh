72008eb706b92f2715ad3f7fb83bddfc340abf58
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2021-01-02 16:19:45 +0100

    Move to port 80

--- docker_rebuild.sh
+++ docker_rebuild.sh
@@ -5,8 +5,8 @@ set -e
 
 # git pull
 
-docker build -t eu.gcr.io/magnetic-tenure-300523/dog80:latest .
+docker build -t dog .
 
 docker container rm --force dog_container || true
 
-docker run --rm -it -p 80:80 --name dog_container eu.gcr.io/magnetic-tenure-300523/dog80
+docker run --rm -it -p 80:80 --name dog_container dog
